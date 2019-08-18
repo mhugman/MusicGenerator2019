b4365fddb07d6afd231c393562a89642ec54587d
Michael Hugman <mhugman3@gatech.edu>
2019-08-18 15:58:51 -0500

    working on parseMidi

--- MusicGenerator2019.py
+++ MusicGenerator2019.py
@@ -21,11 +21,14 @@ noteArray = np.random.randint(0, 128, size=(NUM_TRACKS, SONG_LENGTH))
 velocityArray = np.random.randint(0, 128, size=(NUM_TRACKS, SONG_LENGTH))
 onOffArray = np.random.randint(-1, 2, size=(NUM_TRACKS, SONG_LENGTH))
 
-midiFunctions.createMidi(noteArray, velocityArray, onOffArray, int(round(60000000 / TEMPO)))
+midiFunctions.createMidi(noteArray, velocityArray, onOffArray, int(round(60000000 / TEMPO)), "new_song")
 
-mid = mido.MidiFile('midi/new_song.mid')
+noteArray_mario, velocityArray_mario, onOffArray_mario = midiFunctions.parseMidi(mido.MidiFile('midi/mario.mid'))
 
+midiFunctions.createMidi(noteArray_mario, velocityArray_mario, onOffArray_mario, int(round(60000000 / TEMPO)), "new_mario")
 
+#mid = mido.MidiFile('midi/new_song.mid')
+mid = mido.MidiFile('midi/new_mario.mid')
 #mid = MidiFile('mario.mid')
 
 playMidi(mid)
--- midiFunctions.py
+++ midiFunctions.py
@@ -7,11 +7,101 @@ from mido.midifiles import MidiTrack
 from mido import MetaMessage
 from mido import Message
 
-def midiToArray(): 
+def parseMidi(mid): 
 
-    pass
+    # Initialize the note Array 
+    noteArray = np.zeros((1,1,1), dtype=(int,3))
+        
+    # First build the note Array vertically (one row for each track, excluding the one
+    # track we already accounted for in initializing the array
+    # go through the messages and add up all the delta times of the messages, so we can 
+    # figure out the maximum track length in clicks. This will let us us know how
+    # far to build out the array horizontally
+    
+    maxTrackLength = 0
+    for i, track in enumerate(mid.tracks[1:]):
+        
+        trackLength = 0
+        for message in track:
+            trackLength = trackLength + message.time
+            
+        if trackLength > maxTrackLength: 
+            maxTrackLength = trackLength
+        if i > 0: 
+            noteArray = np.vstack((noteArray, [[[(0,0,0)]]]))
+            
+    #raise ValueError(noteArray)
+            
+    #prettyPrintArray(noteArray)
+    
+    # Build the array horizontally, by adding empty vertical vectors for the length
+    # of the longest track
+        
+    emptyVector = np.zeros((noteArray.shape[0],1,1), dtype=(int,3))
+    
+    #raise ValueError(maxTrackLength)
+    
+    for x in range(maxTrackLength): 
+        noteArray = np.hstack((noteArray, emptyVector))
+    
+    #print(noteArray.shape)   
+    #prettyPrintArray(noteArray)
+        
+    
+    for i, track in enumerate(mid.tracks[1:]):
+    
+        #print 'Track {}: {}'.format(i, track.name)
+        
+        currentNotes = []
+        
+        if track.name not in exclusions: 
+            
+            currentTime = 0
+            for message in track:
+                
+                if message.type == "note_on" or message.type == "note_off":
+                    prevTime = currentTime
+                    currentTime = currentTime + message.time
+                    
+                    # previous notes are the same as (not yet updated) current notes, 
+                    # but the third value is 0 instead of 1 (denoting the fact that 
+                    # the note is sustained, rather than hit)
+                    prevNotes = []
+                    for x in currentNotes: 
+                         prevNotes.append((x[0], x[1], 0))
+                    
+                    # Fill in all the values since the previous message, and up to 
+                    # but not including the time of the current message, with the
+                    # previous notes
+                    if len(prevNotes) > 0:     
+                        deltaTime = currentTime - prevTime
+                        for j in range(0, deltaTime - 1):     
+                            noteArray[i][currentTime - 1 - j] = prevNotes
+                    
+                    # update the current Notes being played with the information in the
+                    # message
+                    if message.type == "note_on": 
+                                
+                        currentNotes.append((message.note + 1, message.velocity,1))
+                    
+                    elif message.type == "note_off": 
+                        for x in currentNotes: 
+                            if x[0] == message.note + 1: 
+                                 currentNotes.remove(x)
+                        #currentNotes.remove((message.note + 1, message.velocity))   
+                     
+                    # fill in the value for this particular time with the current Notes
+                    if len(currentNotes) > 0:    
+                        try:  
+                            noteArray[i][currentTime] = asarray(currentNotes)
+                        except: 
+                            raise ValueError(message, i, currentTime, currentNotes)
+                        
+    
+    #raise ValueError(noteArray.shape[1])            
+    return noteArray
 
-def createMidi(noteArray, velocityArray, onOffArray, TEMPO): 
+def createMidi(noteArray, velocityArray, onOffArray, TEMPO, filename): 
 
     '''
     This function will take a 3D numpy note Array (e.g. parsed from a midi file), and turn it into 
@@ -96,7 +186,7 @@ def createMidi(noteArray, velocityArray, onOffArray, TEMPO):
                     # if there was no message, simply increment the time since the last message
                     timeSinceLastMessage = timeSinceLastMessage + 1
 
-        mid.save('midi/new_song.mid')
+        mid.save('midi/' + filename + '.mid')
 
 
 
